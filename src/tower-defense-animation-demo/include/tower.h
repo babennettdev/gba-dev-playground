5851a2cd0e44e588fc288326b591961ab31ca961
babennettdev <babennettdev@gmail.com>
2021-11-07 14:02:02 -0500

    Updating tower.h for new functions and constants

--- src/tower-defense-animation-demo/include/tower.h
+++ src/tower-defense-animation-demo/include/tower.h
@@ -2,10 +2,16 @@
 #define __TOWER_H__
 
 #include <tonc.h>
-#include <robot.h>
+#include "robot.h"
 // --------------------------------------------------------------------
 // CONSTANTS
 // --------------------------------------------------------------------
+
+#define TOWER_BASE_OBJ_BUFFER_POS 0
+#define TOWER_BASE_TID TOWER_BASE_OBJ_BUFFER_POS * 8
+
+#define TOWER_TURRET_OBJ_BUFFER_POS 1
+#define TOWER_TURRET_TID TOWER_TURRET_OBJ_BUFFER_POS * 8
 
 // --------------------------------------------------------------------
 // TYPES
@@ -13,7 +19,8 @@
 
 typedef struct Tower
 {
-  const int position[2];
+  const int position_x;
+  const int position_y;
   Robot *target;
 } Tower;
 
@@ -23,9 +30,12 @@ typedef struct Tower
 
 void tower_create();
 
-void tower_init();
+void tower_init(OBJ_ATTR *towerBase, OBJ_ATTR *towerTurret, int tower_starting_pos_x, int tower_starting_pos_y);
 void tower_run();
 void tower_draw();
+
+void calculate_target();
+void damage_target();
 
 #endif // __TOWER_H__
 
