eaeed2935167fde5a511d80b95c51bddbe1910ac
babennettdev <babennettdev@gmail.com>
2021-11-07 13:21:53 -0500

    Added some basic constants to keep code DRY and readable

---
+++ src/tower-defense-animation-demo/include/basicConstants.h
@@ -0,0 +1,19 @@
+#ifndef __BASIC_CONSTANTS_H__
+#define __BASIC_CONSTANTS_H__
+
+// --------------------------------------------------------------------
+// CONSTANTS
+// --------------------------------------------------------------------
+
+#define TOTAL_PIXELS_X 240
+#define TOTAL_PIXELS_Y 180
+
+#define CENTER_PIXEL_X (TOTAL_PIXELS_X / 2) - 1
+#define CENTER_PIXEL_Y (TOTAL_PIXELS_Y / 2) - 1
+
+#define CENTER_TILE_X CENTER_PIXEL_X - 16
+#define CENTER_TILE_Y CENTER_PIXEL_Y - 16
+
+#endif // __BASIC_CONSTANTS_H__
+
+// EOF
